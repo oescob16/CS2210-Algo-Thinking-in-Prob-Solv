d52585d2d5f69b26513f539b0a3c13ebcbdfd73f
Oswaldo Escobedo <44388530+oescob16@users.noreply.github.com>
2020-12-03 00:27:34 -0700

    Update exam2_answers.py

--- Exam 2 - Dynamic Programming/exam2_answers.py
+++ Exam 2 - Dynamic Programming/exam2_answers.py
@@ -21,25 +21,41 @@ def minimum_fall_path_sum(A):
     print_dp(dp)
     return np.min(dp[-1:][:]) # Get minimum fall path (cumulative) sum
 
+def arithmetic_slices(A):
+    n = len(A)
+    dp=[0]*n
+    for i in range(2,n):
+        if A[i]-A[i-1]==A[i-1]-A[i-2]: # Valid arithmetic sequence
+            dp[i]= dp[i-1]+1
+    cumulative=0
+    for i in range(n):
+        cumulative+=dp[i] # Sum the backtrack sum.
+    return cumulative
+
 def min_ascii_del_sum(s1,s2):
     n1,n2=len(s1)+1,len(s2)+1
     dp=[[0]*n2 for _ in range(n1)]
     for c in range(1,n2):
-        dp[0][c]=dp[0][c-1]+ord(s2[c-1])
+        dp[0][c]=dp[0][c-1]+ord(s2[c-1]) # Fill first row
     for c in range(1,n1):
-        dp[c][0]=dp[c-1][0]+ord(s1[c-1])
+        dp[c][0]=dp[c-1][0]+ord(s1[c-1]) # Fill first column
     for r in range(1,n1):
         for c in range(1,n2):
-            if s1[r-1]==s2[c-1]:
+            if s1[r-1]==s2[c-1]: # Characters match, grab the top left value.
                 dp[r][c]=dp[r-1][c-1]
-            else:
-                dp[r][c]=min(dp[r][c-1]+ord(s2[c-1]),
-                             dp[r-1][c]+ord(s1[r-1]))
+            else: # Mismatch, grab the minimum 2 prev computation (left and top), min(left+s2[r-1],top+s1[c-1]) 
+                dp[r][c]=min(dp[r-1][c]+ord(s1[r-1]), 
+                             dp[r][c-1]+ord(s2[c-1]))
     return dp[-1][-1]
 
 print(minimum_fall_path_sum([[1,2],[4,5]]))
 print(minimum_fall_path_sum([[1,2,3],[4,5,6],[7,8,9]]))
 print(minimum_fall_path_sum([[0,1,0],[3,0,3],[9,5,2]]))
+
+print(arithmetic_slices([1,3,5,7,9]))
+print(arithmetic_slices([1,2,3,4]))
+print(arithmetic_slices([1,2,3,4,5]))
+print(arithmetic_slices([1,3,5,8,10]))
 
 print(min_ascii_del_sum('',''))
 print(min_ascii_del_sum('e','e'))
