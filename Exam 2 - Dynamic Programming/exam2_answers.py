88a5a333abbbfe3baeea02e2ad3ca25ee6ccab79
Oswaldo Escobedo <44388530+oescob16@users.noreply.github.com>
2020-12-03 21:52:07 -0700

    Update exam2_answers.py

--- Exam 2 - Dynamic Programming/exam2_answers.py
+++ Exam 2 - Dynamic Programming/exam2_answers.py
@@ -35,7 +35,7 @@ def palindromic_substr(s):
             elif bigger_than_2 and same_char: 
                 dp[start][end] = int(same_char)
             else:
-                dp[start][end] = 0
+                dp[start][end] = -1
     return sum(row.count(1) for row in dp)
 
 def arithmetic_slices(A):
@@ -65,6 +65,24 @@ def min_ascii_del_sum(s1,s2):
                              dp[r][c-1]+ord(s2[c-1]))
     return dp[-1][-1]
 
+def integer_break(n):
+    if n==0:
+        return 0
+    if n<=2:
+        return 1
+    if n==3:
+        return 2
+    dp=[0]*(n+1)
+    dp[1]=dp[2]=1
+    dp[3]=2
+    for i in range(2,n+1):
+        for j in range(i):
+            prev_max_parent=dp[i] # prev max of i
+            prev_max_child=dp[i-j]*j # prev max from one of the childs of i
+            new_max_child=(i-j)*j # new max computed
+            dp[i]=max(prev_max_parent,prev_max_child,new_max_child)
+    return dp[-1]
+
 print(minimum_fall_path_sum([[1,2],[4,5]]))
 print(minimum_fall_path_sum([[1,2,3],[4,5,6],[7,8,9]]))
 print(minimum_fall_path_sum([[0,1,0],[3,0,3],[9,5,2]]))
@@ -88,3 +106,6 @@ print(min_ascii_del_sum('e','a'))
 print(min_ascii_del_sum('ee','a'))
 print(min_ascii_del_sum('eat','sea'))
 print(min_ascii_del_sum('delete','leet'))
+
+for i in range(10):
+    print(integer_break(i+1))
